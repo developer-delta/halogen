3ca5986b8fc703c98880b8522f1e74aa5a97eb0b
Hanna-N9 <hannyhabte@yahoo.com>
2021-07-09 11:47:44 -0700

    Deleting the files

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
-const app = express();
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
