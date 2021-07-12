d4b1c5a59a4c1ed43c9ab91004482414c8b9e6d1
Hanna-N9 <hannyhabte@yahoo.com>
2021-07-11 22:41:54 -0700

    #33 - Change to App.js by making an HTTP request

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  /* Fetch API is a tool that manipulates HTTP and, at this moment is to perform requests. This is using a Get request, so to 
+   that is, take in a promise which is a path of /api address that we want to fetch from and make our request. 
+   So basically to fetch information from /api address. */
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json()) //return the body as a promise and, as a result, parse it as JSON.
+      .then((data) => setData(data.message)); //take the message property that we had sent from the server.
+  }, []);
+
+  /* JSX is a syntax extension for JavaScript. It looks like a lot of HTML used for React. Currently, the codes show the
+    curly braces, which is to treat the code located between JSX tags inside the curly braces as markers of the beginning 
+    and end of a JavaScript injection into JSX like a quotation for strings.  Because without the curly braces, any code 
+    between tags of a JSX element will be read as text like HTML and not as JavaScript. */
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data}</p>
+        {/* If no data message, then it will load otherwise, there is a data message */}
       </header>
     </div>
   );
