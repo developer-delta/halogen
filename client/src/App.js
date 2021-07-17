daaa805ac6cb90d795593094f4a374561ecbf608
aggarwalsshreya@gmail.com <aggarwalsshreya@gmail.com>
2021-07-16 17:26:19 -0700

    cleaned repo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -19,7 +18,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>{" "}
         {/* If no data message, then it will load otherwise, there is a data message */}
       </header>
