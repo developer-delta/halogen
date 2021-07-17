2a77c534e1352776a76d15084d483aff5c1c5ab9
rohinij15 <roh3290a@gmail.com>
2021-07-17 16:35:48 -0700

    setup mongoose

--- server/index.js
+++ server/index.js
@@ -5,6 +5,16 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+//Mongoose setup code below
+
+// getting-started.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 app.get("/api", (req, res) => {
   //json stands for javascript object notation (data type)
