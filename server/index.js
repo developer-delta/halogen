977b3c09277ab08706f66db4f1d1c06d1c708414
rohinij15 <roh3290a@gmail.com>
2021-07-17 17:14:12 -0700

    setup mongoose

--- server/index.js
+++ server/index.js
@@ -9,11 +9,36 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // getting-started.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.HALOGEN_WEB || 'mongodb+srv://hnegash:AiVb0JWV7MOejVIt@halogen-west.d2kkg.mongodb.net/showtracker?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+  //Do we keep this kitty stuff or should we rewrite it?
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+  const Kitten = mongoose.model('Kitten', kittySchema);
+  const silence = new Kitten({ name: 'Silence' });
+  console.log(silence.name); // 'Silence'
+  kittySchema.methods.speak = function () {
+    const greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  }
+  
+  const Kitten = mongoose.model('Kitten', kittySchema);
+  const fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.speak(); // "Meow name is fluffy"
+  fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+  });
+  Kitten.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  })
+  Kitten.find({ name: /^fluff/ }, callback);
 });
 
 app.get("/api", (req, res) => {
