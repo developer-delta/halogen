8fd2435004d86b6e2ba7b9d9b47ed5575dd67f16
Hanna-N9 <hannyhabte@yahoo.com>
2021-07-24 18:24:36 -0700

    Changed the component

--- client/src/TextInput.js
+++ client/src/TextInput.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
-class TextInput extends Component {
-  render() {
-    return (
-      <div>
-        <div className="mt-5 mb-2">
-          <input
-            className="bg-gray-600 placeholder-gray-400 px-5 py-3 rounded text-lg border border-gray-400"
-            type="text"
-            placeholder="Email address"
-          />
-        </div>
-        <div>
-          <input
-            className="bg-gray-600 placeholder-gray-400 px-5 py-3 rounded text-lg border border-gray-400"
-            type="text"
-            placeholder="Password"
-          />
-        </div>
+function TextInput() {
+  return (
+    <div>
+      <div className="mt-5 mb-2">
+        <input
+          className="bg-gray-600 placeholder-gray-400 px-5 py-3 rounded text-lg border border-gray-400"
+          type="text"
+          placeholder="Email address"
+        />
       </div>
-    );
-  }
+      <div>
+        <input
+          className="bg-gray-600 placeholder-gray-400 px-5 py-3 rounded text-lg border border-gray-400"
+          type="text"
+          placeholder="Password"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default TextInput;
