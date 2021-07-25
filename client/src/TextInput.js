687d358522a5b0abe529a58647727660a0d0f222
Hanna-N9 <hannyhabte@yahoo.com>
2021-07-24 18:15:36 -0700

    Put styles for text input separately

--- client/src/TextInput.js
+++ client/src/TextInput.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 
-var inputStyle =
-  "bg-gray-600 placeholder-gray-400 px-5 py-3 rounded text-lg border border-gray-400";
-
 class TextInput extends Component {
   render() {
     return (
       <div>
         <div className="mt-5 mb-2">
           <input
-            className={inputStyle}
+            className="bg-gray-600 placeholder-gray-400 px-5 py-3 rounded text-lg border border-gray-400"
             type="text"
             placeholder="Email address"
           />
         </div>
         <div>
-          <input className={inputStyle} type="text" placeholder="Password" />
+          <input
+            className="bg-gray-600 placeholder-gray-400 px-5 py-3 rounded text-lg border border-gray-400"
+            type="text"
+            placeholder="Password"
+          />
         </div>
       </div>
     );
