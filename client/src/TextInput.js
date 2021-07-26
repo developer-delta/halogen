3ea7d00ecbe2c5904865e1eeddfcfc353a54a4db
Hanna-N9 <hannyhabte@yahoo.com>
2021-07-26 13:11:18 -0700

    Remove a term 'input' in TextInput.js

--- client/src/TextInput.js
+++ client/src/TextInput.js
@@ -4,7 +4,6 @@ function TextInput({ placeholder }) {
   return (
     <div>
       <input
-        input
         className="bg-gray-600 placeholder-gray-400 px-5 py-3 rounded text-lg border border-gray-400 mt-2"
         type="text"
         placeholder={placeholder}
