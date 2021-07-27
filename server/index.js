07dd1f5469b8e1a827a86b666b3c87c69969f3dd
rohinij15 <roh3290a@gmail.com>
2021-07-26 21:25:54 -0700

    I have deleted all the Kitty stuff and changed the url also

--- server/index.js
+++ server/index.js
@@ -14,35 +14,10 @@ mongoose.connect(process.env.HALOGEN_WEB || 'mongodb://localhost:3001/showtracke
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  //Do we keep this kitty stuff or should we rewrite it?
-  const kittySchema = new mongoose.Schema({
-    name: String
-  });
-  const Kitten = mongoose.model('Kitten', kittySchema);
-  const silence = new Kitten({ name: 'Silence' });
-  console.log(silence.name); // 'Silence'
-  kittySchema.methods.speak = function () {
-    const greeting = this.name
-      ? "Meow name is " + this.name
-      : "I don't have a name";
-    console.log(greeting);
-  }
-  
-  const Kitten = mongoose.model('Kitten', kittySchema);
-  const fluffy = new Kitten({ name: 'fluffy' });
-  fluffy.speak(); // "Meow name is fluffy"
-  fluffy.save(function (err, fluffy) {
-    if (err) return console.error(err);
-    fluffy.speak();
-  });
-  Kitten.find(function (err, kittens) {
-    if (err) return console.error(err);
-    console.log(kittens);
-  })
-  Kitten.find({ name: /^fluff/ }, callback);
+  console.log("It's working!");
 
 });
-
+  
 app.get("/api", (req, res) => {
   //json stands for javascript object notation (data type)
   res.json({ message: "Halogen under construction" });
