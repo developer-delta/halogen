cf5021d3dea8dc8d294cc63515d07da99cf240f0
Hanna-N9 <hannyhabte@yahoo.com>
2021-08-04 15:11:10 -0700

    Removed a float css in Button.js

--- client/src/Button.js
+++ client/src/Button.js
@@ -4,7 +4,7 @@ function Button(props) {
   return (
     <button
       {...props}
-      className="bg-gray-500 text-white px-4 py-1 rounded-full float-right mr-10 text-base"
+      className="bg-gray-500 text-white px-4 py-1 rounded-full mr-10 text-base"
     >
       {props.text}
     </button>
