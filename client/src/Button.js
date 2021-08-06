cc000ea6743c79b415f186245bc314efe2d18710
rohinij15 <roh3290a@gmail.com>
2021-08-05 22:40:30 -0700

    designed the hero section. Worked with tailwind aand flex to align the elements on the webapp

--- client/src/Button.js
+++ client/src/Button.js
@@ -3,7 +3,8 @@ import React from "react";
 function Button(props) {
   return (
     <button
-      {...props}
+    style={{ width: props.width, height: props.height, fontSize: props.fontSize }}  
+    {...props}
       className="bg-gray-500 text-white px-4 py-1 rounded-full float-right mr-10 text-base"
     >
       {props.text}
