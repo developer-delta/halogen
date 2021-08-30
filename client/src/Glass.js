7829f4ed7826ae1b855beb4c8733d986f4e513ab
Hanna-N9 <hannyhabte@yahoo.com>
2021-08-30 16:02:11 -0700

    Added change in Glass.js

--- client/src/Glass.js
+++ client/src/Glass.js
@@ -9,7 +9,7 @@ function Glass(props) {
         padding: props.padding,
       }}
       {...props}
-      className="glass rounded-full px-4 py-1 flex items-center justify-center"
+      className="rounded-full px-4 py-1 flex items-center justify-center backdrop-filter backdrop-blur bg-dark-glass-grey "
     ></div>
   );
 }
